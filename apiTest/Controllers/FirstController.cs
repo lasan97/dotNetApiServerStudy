e8acf6fc8a9de71f993609a5f76fb95764bccf9f
lasan97 <fktks97@naver.com>
2026-06-17 18:57:00 +0900

    first API & Swagger

---
+++ apiTest/Controllers/FirstController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiTest.Controllers;
+
+/// <summary>
+/// class 이름이 java랑 다르게 파일 이름과 같을 필요가 없음
+/// ControllerBase는 API 구성에 필요한 유틸들이 있음
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class FirstController: ControllerBase
+{
+    [HttpGet]
+    public ActionResult<string> Hello()
+    {
+        return Ok("Hello Api Test");
+    }
+}
--- apiTest/Program.cs
+++ apiTest/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Controller 등록
+builder.Services.AddControllers();
+
+// Swagger 등록
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+// 이것은 Minimal API
+app.MapGet("/", () =>
+{
+    return Results.Ok("Hello World!");
+});
 
 app.Run();
